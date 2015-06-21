131e61d219f2c2697ee552a0e115e545385d18e2
Michael Pascuzzi <gatorpazz@gmail.com>
2015-06-21 03:48:06 -0400

    added functionality to homepage

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 (function(window) {
     var $rightNavLink = $('#rightNavBar a');
     var $blogs = $('.blog');
+    var $home = $('#home');
+    var $homepage = $('#homepage');
 
     $rightNavLink.click(function() {
         var $bridge = $($(this).attr('href'));
         $blogs.addClass('hidden');
+        $homepage.addClass('hidden');
         $bridge.removeClass('hidden');
     });
+
+    $home.click(function() {
+      $blogs.addClass('hidden');
+      $homepage.removeClass('hidden');
+    })
 
 })(window);
