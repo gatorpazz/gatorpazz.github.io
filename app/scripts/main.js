4a8c6371ac13c0d4110352064d3cd8133626d76a
Michael Pascuzzi <gatorpazz@gmail.com>
2016-01-31 17:23:57 -0500

    changing the structure, adding the initial js to get it working

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+$('nav li').click(function(){
+  $('nav li').removeClass('navActive');
+  $('section').removeClass('sectionActive');
+  $(this).addClass('navActive');
+  var dataSection = $(this).attr('data-section');
+  var section = $('section[data-section' + dataSection + ']')
+  $(section).addClass('sectionActive');
+})
