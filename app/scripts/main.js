4b2c9a35452205c91737ae3dd178079dd0f6a4d7
Michael Pascuzzi <gatorpazz@gmail.com>
2016-05-12 11:07:25 -0400

    Added stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-angular.module('Portfolio', ['ngMaterial']);
+angular
+    .module('Portfolio', ['ngMaterial'])
+    .config(function($mdThemingProvider){
+      $mdThemingProvider.theme('custom1Theme')
+        .primaryPalette('orange')
+        .accentPalette('blue')
+        .warnPalette('pink');
+      $mdThemingProvider.theme('custom2Theme')
+          .primaryPalette('green')
+          .accentPalette('lime')
+          .warnPalette('pink')
+          .dark();
+      $mdThemingProvider.alwaysWatchTheme(true);
+    });
