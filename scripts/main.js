e6c161d8d6140eaf373aba0232afdb2fc1b5835a
Michael Pascuzzi <gatorpazz@gmail.com>
2016-05-16 15:55:20 -0400

    Don't understand

--- scripts/main.js
+++ scripts/main.js
@@ -7,5 +7,5 @@ angular
           .dark();
     })
     .config(function($mdIconProvider) {
-      $mdIconProvider.defaultIconSet('/images/mdi.svg', 50)
+      $mdIconProvider.defaultIconSet('/images/mdi.svg')
     });
